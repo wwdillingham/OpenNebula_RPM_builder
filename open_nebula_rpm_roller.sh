f9c495a423af5f3663d426c5f8081f24def730ae
Wes <wes_dillingham@harvard.edu>
2015-07-08 16:33:57 -0400

    initial commit

---
+++ open_nebula_rpm_roller.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#Wes Dillingham
+#wes_dillingham@harvard.edu
+
+#Clone the repo in /tmp
+echo "Update local copy of OpenNebula with remote...."
+sleep 3
+if [ -d /tmp/one ]
+then
+        cd /tmp/one
+        echo "OpenNebula Repository already exists, going to do a git pull to ensure we are up to date with remote"
+        sleep 3
+        git checkout master --quiet
+        git pull
+else
+        echo "Looks like we dont have any repository so going to clone a fresh copy"
+        sleep 3
+        cd /tmp
+        git clone https://github.com/OpenNebula/one.git
+fi
+
+
+echo "****** Remote Update Complete **********"
+git branch
+echo "Generating list of remote branches:.... "
+sleep 3
+cd /tmp/one
+git branch -a | grep -i remote | awk -F '/' '{print $3}'
+echo "Please select a branch that we want to roll into an RPM (example: one-1.0 )"
+read REMOTEBRANCH
+echo "Attempting to checkout branch: $REMOTEBRANCH"
+sleep 3
+git checkout $REMOTEBRANCH
+sleep 2
+
+##Download the most recent sourcepackage subversion
+#############
+echo "We are going to now roll the changes present in the git working repo that you selected into the latest source package"
+echo "release from downloads.opennebula.org. We will swap the source code from the git repo into the package skeleton provided"
+echo "by the bundled package. The result will be a new set of compiled RPMs with the latest feature commits"
+#Determine the BASE VERSION of open nebula we want to roll the git changes into
+GITBASEVERSION=`echo "$REMOTEBRANCH" | awk -F '-' '{print $2}'`
+RELEASEURL=`curl -s http://downloads.opennebula.org/packages/ | grep -i "opennebula\-$GITBASEVERSION" | tail -n 1 | awk -F 'href' '{print $2}' | awk -F '"' '{print $2}' | sed s/.$//`
+
+RELEASEVERSION=`echo "$RELEASEURL" | awk -F '-' '{print $2}'`
+
+echo -e "\nGit Target Version       = $GITBASEVERSION"
+echo -e "Packaged Release Version = $RELEASEVERSION \n"
+echo -e "The above two numbers should be reasonably similar if not CTRL-C"
+sleep 4
+
+echo "Now Downloading the packaged release version from downloads.opennebula.org to /tmp"
+#wget http://downloads.opennebula.org/packages/opennebula-4.12.1/CentOS-7/CentOS-7-opennebula-4.12.1-1.tar.gz
+FULLURL="http://downloads.opennebula.org/packages/$RELEASEURL/CentOS-7/CentOS-7-$RELEASEURL"
+TARGZ=".tar.gz"
+FULLURL=$FULLURL$TARGZ
+
+echo "Full url is $FULLURL"
+wget --directory-prefix=/tmp $FULLURL
