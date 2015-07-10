6032bd0385aa3918e333aa0feb82ad0a3cdf3499
root <root@ceph-osd05.rc.fas.harvard.edu>
2015-07-10 17:01:12 -0400

    ready to run rpmbuild

--- open_nebula_rpm_roller.sh
+++ open_nebula_rpm_roller.sh
@@ -28,6 +28,7 @@ cd /tmp/one
 git branch -a | grep -i remote | awk -F '/' '{print $3}'
 echo "Please select a branch that we want to roll into an RPM (example: one-1.0 )"
 read REMOTEBRANCH
+#TODO Need to validate that it is is a legit branch
 echo "Attempting to checkout branch: $REMOTEBRANCH"
 sleep 3
 git checkout $REMOTEBRANCH
@@ -43,12 +44,6 @@ GITBASEVERSION=`echo "$REMOTEBRANCH" | awk -F '-' '{print $2}'`
 RELEASEURL=`curl -s http://downloads.opennebula.org/packages/ | grep -i "opennebula\-$GITBASEVERSION" | tail -n 1 | awk -F 'href' '{print $2}' | awk -F '"' '{print $2}' | sed s/.$//`
 
 RELEASEVERSION=`echo "$RELEASEURL" | awk -F '-' '{print $2}'`
-
-echo "GITBASEVERSION IS $GITBASEVERSION"
-echo "RELEASEURL IS $RELEASEURL"
-echo "RELEASEVERSION IS $RELEASEVERSION"
-
-
 echo -e "\nGit Target Version       = $GITBASEVERSION"
 echo -e "Packaged Release Version = $RELEASEVERSION \n"
 echo -e "The above two numbers should be reasonably similar if not CTRL-C"
@@ -62,12 +57,45 @@ sleep 4
 
 REMOTETARGZ=`curl -s http://downloads.opennebula.org/packages/$RELEASEURL/CentOS-7/ | grep -i "tar.gz" | awk -F 'href' '{print $2}' | awk -F '"' '{print $2}'`
 FULLURL="http://downloads.opennebula.org/packages/$RELEASEURL/CentOS-7/$REMOTETARGZ"
-echo "Now Downloading the packaged release version from  $FULLURL"
 #TODO: Check if file already exists
 if [ -f /tmp/$REMOTETARGZ ] 
 then
 	echo "ERROR: File already exists, please remove or move it from /tmp/$REMOTETARGZ this script will not generate RPMS from a file it did not download"
+	exit
 else
+	echo "Now Downloading the packaged release version from  $FULLURL"
+	echo "Downloaded filename is $REMOTETARGZ"
 	wget --directory-prefix=/tmp $FULLURL
 fi
+#Now we have the file so we should extract it. 
+sleep 2
+echo "Now extracting the archive we downloaded: /tmp/$REMOTETARGZ"
+sleep 2
+tar xf /tmp/$REMOTETARGZ -C /tmp #$REMOTETARGZ --verbose
+EXTRACTEDSOURCEDIR=`tar -tf  /tmp/$REMOTETARGZ  | grep -v ".rpm" | grep -i src`
+echo "EXTRACTEDSOURCEDIR is $EXTRACTEDSOURCEDIR"
+
+if [ -d /tmp/$EXTRACTEDSOURCEDIR ]
+then
+	echo "Source directory found at /tmp/$EXTRACTEDSOURCEDIR"
+        echo "Now Extracting the source RPM to grab the spec file"
+	cd /tmp/$EXTRACTEDSOURCEDIR
+        rpm2cpio /tmp/$EXTRACTEDSOURCEDIR/*.src.rpm | cpio -idmv 
+	cd /tmp
+else 
+	echo "ERROR: Cant find source directory in downloaded tarball, expected to find it here: /tmp/$EXTRACTEDSOURCEDIR"
+fi
+
+sleep 3
+echo "Now we will build the file structure for an RPM build."
+mkdir -p /tmp/$RELEASEURL-fasrc/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+sleep 2
+echo "Now we will rsync the source from the git branch into our RPM build file structure"
+rsync -ah /tmp/one/src/* /tmp/$RELEASEURL-fasrc/SOURCES
+echo "rsync complete"
+sleep 2
+echo "Now copying the spec file from the downloaded tarball into the build directory"
+cp /tmp/$EXTRACTEDSOURCEDIR/centos7.spec /tmp/$RELEASEURL-fasrc/SPECS
+echo "spec file copy complete"
 
