127dc00a365ba895000afb64dcf6fe6319692eb2
Wes <wes_dillingham@harvard.edu>
2015-07-15 11:25:51 -0400

    forgot to add sed file to act on, and forgot one variable

--- open_nebula_rpm_roller.sh
+++ open_nebula_rpm_roller.sh
@@ -120,10 +120,10 @@ rsync -a --exclude 'opennebula*.tar.gz' /tmp/$EXTRACTEDSOURCEDIR /tmp/opennebula
 echo "rsync complete"
 sleep 2
 echo "Now copying the spec file from the downloaded tarball into the build directory"
-cp /tmp/$EXTRACTEDSOURCEDIR/centos7.spec /tmp/$RELEASEURL-fasrc/SPECS
+cp /tmp/$EXTRACTEDSOURCEDIR/centos7.spec /tmp/opennebula-$RELEASESUBVERSION-$HOUSEVERSION/SPECS
 # sed -i 's/^.*Version\:.*$/Version: 4.12.3-fasrc01/g'
 echo "Applying $RELEASESUBVERSION-$HOUSEVERSION to spec file"
-sed -i 's/^.*Version\:.*$/Version: $RELEASESUBVERSION-$HOUSEVERSION/g' 
+sed -i 's/^.*Version\:.*$/Version: $RELEASESUBVERSION-$HOUSEVERSION/g' /tmp/$RELEASEURL-fasrc/SPECS/centos7.spec
 echo "spec file copy complete"
 sleep 2
 echo "The following dependencies are required for the source compilation / rpm building: rpm-build libcurl-devel libxml2-devel xmlrpc-c-devel mysql-devel sqlite-devel scons java-1.7.0-openjdk-devel log4cpp log4cpp-devel"
