93e9fbc2bcc19de8731486c976409edc02fb2162
Wes <wes_dillingham@harvard.edu>
2015-07-21 11:40:25 -0400

    added userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+env > /tmp/i_ran
+source /mnt/context.sh
+yum install -y git wget rpm-build gcc make yum-utils
+git clone https://github.com/wwdillingham/OpenNebula_RPM_builder /tmp/launch 
+cd /tmp/launch
+bash open_nebula_rpm_roller.sh 2>&1 > /tmp/build.log
